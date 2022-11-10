e3c40edc75d51abb4ed3f9038e8ffeb017a9d25d
thang nguyen <thang.nguyenngoc1996@gmail.com>
2022-11-10 07:24:50 +0700

    save work

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import logo from './bk_logo.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        {/* <img src={logo} className="BK-logo" alt="logo" />
+        <div>
+          BKU
+        </div> */}
+
+
+        <div id="container" style={{whiteSpace:'nowrap'}}>
+          <div id="image" style={{display:'inline'}} align="left">
+              <img src={logo} className="BK-logo" alt="logo" />
+          </div>
+          <div id="texts" style={{display:'inline', whiteSpace:'nowrap'}}> 
+              BKU
+          </div>
+        </div>
+
+
       </header>
     </div>
+
+
   );
 }
 
